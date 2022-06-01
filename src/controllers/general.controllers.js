77eb9950c4665ce3751b1072b9d504880707aad2
BrayanCape <brayancapetoccc@gmail.com>
2022-06-01 11:45:07 -0500

    new changes

--- src/controllers/general.controllers.js
+++ src/controllers/general.controllers.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
+const path = require ('path');
 
 const general={
 
@@ -8,7 +9,6 @@ const general={
 
     getfiles : async (req,res)=>{
         const fs = require('fs');
-
         const urls = "http://localhost:4000/uploads/";
 
         fs.readdir("./uploads",(error,files)=>{
@@ -32,6 +32,190 @@ const general={
 
         res.json(arr)
     })
+    },
+    getfiles : async (req,res)=>{
+        const fs = require('fs');
+        const urls = "http://localhost:4000/uploads/";
+
+        fs.readdir("./uploads",(error,files)=>{
+        if(error){
+            throw error
+        }
+
+        var arr= [];
+            for(var i=0;i<files.length;i++){
+                var ne = new person(files[i].split(".",1).toString(), files[i].split(".").pop(),`${urls}${files[i]}`);
+                arr.push(ne);
+        }
+
+    
+  
+        function person(nam,ext,url){
+        this.name= nam;
+        this.ext = ext;
+        this.url = url;
+        }
+
+        res.json(arr)
+    })
+    },
+    getfiles2 : async (req,res)=>{
+        const fs2 = require('fs');
+
+        var arr= [];
+
+        const urls = "http://localhost:4000/uploads/";
+
+        var ls =fs2.readdirSync("./uploads");
+
+        for(var i=0;i<ls.length;i++){
+            const file  = path.join("./uploads",ls[i]);
+            var dataFile =null;
+
+            try{
+               dataFile =fs2.lstatSync(file);
+            }catch(e){}
+
+            if(dataFile){
+                var ne = new person(ls[i].split(".",1).toString(), ls[i].split(".").pop(),`${urls}${ls[i]}`,dataFile.isDirectory());
+                arr.push(ne);
+             }
+        }
+        
+        function person(nam,ext,url,dir){
+        this.name= nam;
+        this.ext = ext;
+        this.url = url;
+        this.dir = dir;
+        }
+
+
+
+        //ordenar el vector por tipo
+        var arr2=[];
+        var j=0;
+
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].dir==true){
+                arr2[j]=arr[i];
+                j++;
+            }
+        }
+
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].ext=="docx" || arr[i].ext=="doc"){
+                arr2[j]=arr[i];
+                j++
+            }
+        }
+
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].ext=="pdf"){
+                arr2[j]=arr[i];
+                j++
+            }
+        }
+
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].ext=="xlsx" || arr[i].ext=="xls"){
+                arr2[j]=arr[i];
+                j++
+            }
+        }
+
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].ext=="jpg" || arr[i].ext=="png"){
+                arr2[j]=arr[i];
+                j++
+            }
+        }
+
+        res.json(arr2)
+    },
+
+    postrute: async (req,res)=>{
+        
+        console.log(req.body.ruta);
+
+        var arr= [];
+        const fs2 = require('fs');
+        const urls = `http://localhost:4000/uploads/${req.body.ruta}/`;
+
+        var ls =fs2.readdirSync(`./uploads/${req.body.ruta}`);
+
+        for(var i=0;i<ls.length;i++){
+            const file  = path.join(`./uploads/${req.body.ruta}`,ls[i]);
+            var dataFile =null;
+
+            try{
+               dataFile =fs2.lstatSync(file);
+            }catch(e){}
+
+            if(dataFile){
+                var ne = new person(ls[i].split(".",1).toString(), ls[i].split(".").pop(),`${urls}${ls[i]}`,dataFile.isDirectory());
+                arr.push(ne);
+             }
+        }
+        
+        function person(nam,ext,url,dir){
+        this.name= nam;
+        this.ext = ext;
+        this.url = url;
+        this.dir = dir;
+        }
+
+
+
+        //ordenar el vector por tipo
+        var arr2=[];
+        var j=0;
+
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].dir==true){
+                arr2[j]=arr[i];
+                j++;
+            }
+        }
+
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].ext=="docx" || arr[i].ext=="doc"){
+                arr2[j]=arr[i];
+                j++
+            }
+        }
+
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].ext=="pdf"){
+                arr2[j]=arr[i];
+                j++
+            }
+        }
+
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].ext=="xlsx" || arr[i].ext=="xls"){
+                arr2[j]=arr[i];
+                j++
+            }
+        }
+
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].ext=="jpg" || arr[i].ext=="png"){
+                arr2[j]=arr[i];
+                j++
+            }
+        }
+
+        
+        res.json(arr2)
+    
+    },
+    
+    newroute:async (req,res)=>{
+        var rutas = req.body.ruta;
+        const fs = require('fs');
+        console.log(rutas)
+        fs.mkdirSync(`./uploads/${rutas}`,{recursive:true});
+        res.json("ruta creada");
     }
 }
 
--- src/routes/general.routes.js
+++ src/routes/general.routes.js
@@ -5,13 +5,18 @@ const router = Router();
 
 const {
     getfiles,
+    getfiles2,
+    postrute,
     hello,
+    newroute,
     
 } = require('../controllers/general.controllers')
 
 router.get('/getfiles',getfiles);
+router.get('/getfiles2',getfiles2);
+router.post('/postrute',postrute);
+router.post('/newroute',newroute);
 router.get('/',hello);
-
 
 
 module.exports = router;
